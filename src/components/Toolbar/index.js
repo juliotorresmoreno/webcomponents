c460266f7cd4295cfe8e74dd72ae41a495487278
julio torres <jtorres990@gmail.com>
2018-01-03 13:46:59 -0500

    se removieron los comentarios

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -27,10 +27,7 @@ export default class Toolbar extends Component {
                         <ul class="navbar-nav mr-right">
                             <li class="nav-item">
                                 <a class="nav-link" data-toggle="modal" data-target="#loginDialog" href="javascript: void(0)">Sign in</a>
-                            </li>,
-                            {/*<li class="nav-item">
-                                <a class="nav-link" href="#">Register</a>
-                            </li>*/}
+                            </li>
                         </ul>
                     </div>
                 </nav>
@@ -40,54 +37,3 @@ export default class Toolbar extends Component {
     }
 };
 
-
-
-/**
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a class="navbar-brand" href="#">
-                    {this.state.title}
-                </a>
-                <button 
-                    class="navbar-toggler" 
-                    type="button" data-toggle="collapse" 
-                    data-target="#navbarSupportedContent" 
-                    aria-controls="navbarSupportedContent" 
-                    aria-expanded="false" 
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item dropdown">
-                            <a 
-                                class="nav-link dropdown-toggle" 
-                                href="#" id="navbarDropdown" 
-                                role="button" style={{color: 'white'}}
-                                data-toggle="dropdown" 
-                                aria-haspopup="true" 
-                                aria-expanded="false">
-                                Archivo
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a 
-                                    class="dropdown-item" href="#" 
-                                    click={() => console.log('sd')}>
-                                    Nuevo
-                                </a>
-                                <a 
-                                    class="dropdown-item" href="#" 
-                                    click={() => console.log('sd')}>
-                                    Guardar
-                                </a>
-                                <a
-                                    class="dropdown-item" href="#"
-                                    click={() => console.log('sd')}>
-                                    Ejecutar
-                                </a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
- */
